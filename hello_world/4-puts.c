696d3706af0bb0f63586f63c644966cdcc62a46e
mabreu88 <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@github.com>
2023-06-14 10:40:47 -0700

    t4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+		puts("Programming is like building a multilingual puzzle\n");
+		return (0);
+}
