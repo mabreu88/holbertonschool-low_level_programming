6217e1266f38c9e66dac2eec2ea1ce8edf0ed0ca
mabreu88 <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@github.com>
2023-06-14 12:18:26 -0700

    t6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,16 +6,10 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %zu byte(s)\n", sizeof(a));
-	printf("Size of a int: %zu byte(s)\n", sizeof(b));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of a float: %zu byte(s)\n", sizeof(e));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
