8c313cb53fcea6e45f71f5fac0d141713187fbd9
mabreu88 <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@github.com>
2023-06-14 15:08:07 -0700

    t0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("is positive \n");
 	}
 	else if (n == 0)
 	{
-		printf("is zero");
+		printf("is zero \n");
 	}
 	else
 	{
-		printf("is negative");
+		printf("is negative \n");
 	}
 	return (0);
 }
