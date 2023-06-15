05490685afa328356497e78fc8124938540a798b
mabreu88 <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@github.com>
2023-06-15 05:40:55 -0700

    t4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * With the for structure, and the variable char we start counting from the
+ * * low 'a' and increment with ++ until we reach 'z'
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char low;
+
+
+	for (low = 97; low <= 122; low++)
+	{
+		if (low != 101 && low != 113)
+			putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
