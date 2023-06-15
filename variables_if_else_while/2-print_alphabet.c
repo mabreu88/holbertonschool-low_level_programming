2df57558faa64f703c11091404c77b8b7184d475
mabreu88 <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@github.com>
2023-06-14 17:16:37 -0700

    t2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * With the for structure, and the variable char we start counting from the 
+ * low 'a' and increment with ++ until we reach 'z'
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+	return (0);
+}
