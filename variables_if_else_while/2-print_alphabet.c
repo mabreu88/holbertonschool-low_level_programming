16f3be445a22ab15b1fd1cde9a774a9282aad893
mabreu88 <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@github.com>
2023-06-14 17:29:26 -0700

    t2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -13,8 +13,8 @@ int main(void)
 {
 	char low;
 
-	for (low = '97'; low <= '122'; low++)
-		putchar(low),
-		putchar('\n');
+	for (low = 97; low <= 122; low++)
+		putchar(low);
+	putchar('\n');
 	return (0);
 }
