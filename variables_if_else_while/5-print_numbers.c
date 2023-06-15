7c97e78408c2f3d8599cb56fc5bf92fc1cdd405f
mabreu88 <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@github.com>
2023-06-15 06:10:04 -0700

    t5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * With the for structure, and the variable char we start counting from the
+ * * low 'a' and increment with ++ until we reach 'z'
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+		printf("%i", x);
+	printf("\n");
+}
