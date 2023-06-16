0e08e2edf07bfc56a9647100fbdc284b7cddc1bb
mabreu88 <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@github.com>
2023-06-16 05:32:33 -0700

    main.h

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+
