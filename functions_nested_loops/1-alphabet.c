a01f724021c96a44d4920eb9c5f1f568ebc29977
mabreu88 <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@github.com>
2023-06-16 06:05:34 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	char = x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar(\n);
+	return (0);
+}
