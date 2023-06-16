fe1c23200700b881bf38aadfd398db5e70ab5d07
mabreu88 <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@github.com>
2023-06-16 08:56:58 -0700

    t2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printd the aplphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int count;
+
+	for (count = 0; count <= 10; count++)
+	{
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+	}
+}
