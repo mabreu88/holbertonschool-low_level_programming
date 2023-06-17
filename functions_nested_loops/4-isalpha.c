22105aa4910598e67d13155303a0843332687de4
mabreu88 <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@github.com>
2023-06-17 12:51:28 -0700

    t4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - Checker
+ *
+ * @c: Parameter to check
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	char a, b;
+	int x = 0;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		for (b = 'A'; b <= 'Z'; b++)
+		{
+			if (c == a || c == b)
+				x = 1;
+		}
+	}
+	return (x);	
+}
