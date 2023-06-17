1e604de1eb4010b448175188a9577b86acba83af
mabreu88 <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@github.com>
2023-06-17 14:08:51 -0700

    t6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Checker
+ *
+ * @r: Parameter to check
+ *
+ * Return: Absolute of r
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1); 
+}
