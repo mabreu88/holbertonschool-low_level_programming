41c74f99901cd1cd6a226431baceaace718154e0
mabreu88 <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@github.com>
2023-06-19 21:21:01 -0700

    t0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _isupper - Checker
+ * @c: Parameter to check
+ * Return: 1 if c is uppercase
+ */
+int _isupper(int c)
+{
+	char a = 'A';
+	int b = 0;
+
+	for (; a <= 'Z'; a++)
+	{
+		if (c == a)
+		{
+			b = 1;
+			break;
+		}
+	}
+	return (b);
+}
