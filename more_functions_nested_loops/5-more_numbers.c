3e0ba432786d59bf036e88ce86d0691d4d3e404e
mabreu88 <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@github.com>
2023-06-19 22:34:27 -0700

    t5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* more_numbers - Checker
+*
+* Return: Always 0
+*/
+void more_numbers(void)
+{
+	int a, b = 0;
+
+	for (; b < 10; b++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (i > 9)
+				_putchar(a / 10 + '0');
+
+			_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
