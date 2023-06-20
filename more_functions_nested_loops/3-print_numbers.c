9cd22dc2e88b1f03e04db645b8480acb944b0e02
mabreu88 <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@github.com>
2023-06-19 22:10:57 -0700

    t3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Checker.
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int x = '0';
+
+	for (; x <= '9'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
