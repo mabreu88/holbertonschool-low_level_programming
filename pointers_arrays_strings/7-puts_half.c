608afe549720393af401ed680e906e9421c7632b
mabreu88 <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@github.com>
2023-06-23 00:01:46 -0700

    t7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - Checker
+ *
+ *@str: Parameter to check
+ *
+ */
+void puts_half(char *str)
+{
+	int len = 0, x = 0, y = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		x = len / 2;
+	else
+		x = (len - 1) / 2;
+
+	for (y = x; y <= len; y++)
+		_putchar(str[y]);
+	_putchar('\n');
+}
