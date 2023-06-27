b15c59075828f29bb7d3b0bdbdcf9e41c01cbd9c
mabreu88 <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@github.com>
2023-06-27 13:52:35 -0700

    t3x3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strspn - Checker
+ *
+ *@s: A string
+ *
+ *@accept: A char
+ *
+ * Return: Returns the number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int cont = 0, check;
+	char *go = accept;
+
+	while (*s)
+	{
+		check = 0;
+		while (*go)
+		{
+			if (*go == *s)
+			{
+
+				cont++;
+				check = 1;
+				break;
+			}
+			accept++;
+		}
+		s++;
+		accept = go;
+		if (check == 0)
+			break;
+	}
+	return (cont);
+}
