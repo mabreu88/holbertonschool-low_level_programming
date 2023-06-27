47d96c523ae135066956d915dedad4f4cf1518ae
mabreu88 <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@github.com>
2023-06-26 17:33:56 -0700

    t6x2

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,11 +8,10 @@ char *cap_string(char *s)
 {
 	int x = 0;
 
-	if (s[x] >= 'a' && s[x] <= 'z')
-		s[x] -= 32;
-
-	for (x = 1; s[x] != '\0'; x++)
+	while (s[x])
 	{
+		while (!(s[x] >= 'a' && s[x] <= 'z'))
+			x++;
 	if (s[x - 1] == ' ' ||
 			s[x - 1] == '\t' ||
 			s[x - 1] == '\n' ||
@@ -29,6 +28,7 @@ char *cap_string(char *s)
 	{
 		s[x] -= 32;
 	}
+		x++;
 	}
 	return (s);
 }
