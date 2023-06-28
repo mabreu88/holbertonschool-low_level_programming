97f7598a9702f646fa21ef2e454f53d99f585db2
mabreu88 <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@github.com>
2023-06-27 18:42:39 -0700

    t6x3

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - Checker
+ *
+ * @a: An array to print
+ *
+ * Return: Not (https://www.youtube.com/watch?v=rgc_LRjlbTU&ab_channel=ChessVEVO)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j;
+	
+	for (; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
