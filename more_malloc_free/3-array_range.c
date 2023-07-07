05d0dd980ecc972ad55cf2e4c0f54e7693249402
mabreu88 <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@github.com>
2023-07-07 14:30:04 -0700

    t3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,23 +10,23 @@ int *array_range(int min, int max)
 {
 	int *xar;
 	int x, y;
-/**
+/*
  * Check if min is greater than max
  */
 	if (min > max)
 		return (NULL);
-/**
- * Calculates the difference between max and min and 
+/*
+ * Calculates the difference between max and min and
  * stores it in the x variable.
  */
 	x = max - min;
-/**
+/*
  * We allocate the amount of memory to contain x, +1 for the null
  */
 	xar = malloc((x + 1) * sizeof(int));
 	if (xar == NULL)
 		return (NULL);
-/**
+/*
  * We have the value of x, while in xar[y] we equal it to min and increment
  *  it by 1 at each turn of the for, until we add up the value of x
  *  to get all the necessary values.
