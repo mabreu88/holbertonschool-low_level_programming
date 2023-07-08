135af20355618be6a50ddedd00197bf600a3de25
mabreu88 <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@github.com>
2023-07-08 14:23:44 -0700

    t0 with comments

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,9 +1,18 @@
-/* To check if the macro was not previously defined */
+/*
+ *  To check if the macro was not previously defined 
+ */
 #ifndef _OBJECT_LIKE_MACRO_H_
-/* It is used to define it, so we avoid including it many times.
- * This way the content is processed only once. */
+/*
+ * It is used to define it, so we avoid including it many times.
+ * This way the content is processed only once.
+ */
 #define _OBJECT_LIKE_MACRO_H_
-/* SIZE = 1024, Each time SIZE is used, it will automatically
-be set to 1024. Ex: 1+SIZE = 1+1024 */
+/*
+ *  SIZE = 1024, Each time SIZE is used, it will automatically
+ *be set to 1024. Ex: 1+SIZE = 1+1024
+ */
 #define SIZE 1024
-#endif /* Marks the end of the macro _OBJECT_LIKE_MACRO_H_*/
+#endif
+/* 
+ * Marks the end of the macro _OBJECT_LIKE_MACRO_H_
+ */
