75562711498cbab0b27b90f2f612f7211eddc30a
mabreu88 <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@github.com>
2023-07-08 15:29:11 -0700

    t3 with comments

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,10 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
+/*
+ * IF x is less than zero we will reject it using the minus unary '-(x)'
+ * to obtain the result, if x is greater than or equal to 0
+ * we do not make any changes.
+ * For the rest of the comments see 0-object_like_macro.h
+ */
 #define ABS(x) ((x) < (0) ? -(x) : (x))
 #endif
