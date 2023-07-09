1f68c0e21ef73a9c91bcd4eb3ae82b659f0d8064
mabreu88 <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@github.com>
2023-07-09 13:32:43 -0700

    t4 with comments

--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,4 +1,8 @@
 #ifndef _SUM_H
 #define _SUM_H
+/**
+ * We take the values 'x' and 'y' and define the sum of them
+ * For the rest of the comments see 0-object_like_macro.h
+ */
 #define SUM(X, Y) ((X) + (Y))
 #endif
