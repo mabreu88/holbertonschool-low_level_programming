87dee6499a2b0206aceccb83b4e2d4cca8a2fdbe
mabreu88 <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@github.com>
2023-07-10 08:06:56 -0700

    t0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog info
+ * @name: First member 
+ * @age: Second member
+ * @owner: Third member
+ * Description: Dog data.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
