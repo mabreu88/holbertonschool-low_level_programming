dbc428261b487fdaae27544a31d8ee0a179addee
mabreu88 <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@github.com>
2023-07-12 07:59:29 -0700

    t4 new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,16 +15,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	while (name[lname])
 		lname++;
 	while (owner[lowner])
 		lowner++;
-
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
 	copyname = malloc(lname + 1);
 	if (copyname == NULL)
 	{
@@ -34,7 +31,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (x = 0; name[x]; x++)
 		copyname[x] = name[x];
 	copyname[x] = '\0';
-
 	copyowner = malloc(lowner + 1);
 	if (copyowner == NULL)
 	{
@@ -45,7 +41,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (x = 0; owner[x]; x++)
 		copyowner[x] = owner[x];
 	copyowner[x] = '\0';
-
 	new_dog->name = copyname;
 	new_dog->age = age;
 	new_dog->owner = copyowner;
