ef553b5905268c0b24165024982635f0e1ae6fc4
mabreu88 <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@github.com>
2023-07-12 06:54:05 -0700

    t4 new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *new_dog - Function that creates a new dog.
+ *@name: Dog name
+ *@age: Dog age
+ *@owner: Dog owner
+ *Return: new_dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *copyname, *copyowner;
+	int lname = 0, lowner = 0, x;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	while (name[lname])
+		lname++;
+	while (owner[lowner])
+		lowner++;
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	copyname = malloc(sizeof(lname + 1));
+	if (copyname == NULL)
+		return (NULL);
+	for (x = 0; name[x]; x++)
+		copyname[x] = name[x];
+	copyname[x] = '\0';
+	copyowner = malloc(sizeof(lowner + 1));
+	if (copyowner == NULL)
+		return (NULL);
+	for (x = 0; owner[x]; x++)
+		copyowner[x] = owner[x];
+	copyowner[x] = '\0';
+	new_dog->name = copyname;
+	new_dog->age = age;
+	new_dog->owner = copyowner;
+	return (new_dog);
+}
