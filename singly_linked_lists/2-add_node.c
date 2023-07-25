487d382a71453ed84870e51c8c39687bc87034c1
mabreu88 <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@github.com>
2023-07-25 11:44:51 -0700

    add coments fix syntax error

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- *add_node - Function to add node at the head 
+ *add_node - Function to add node at the head
  *@head: Pointer to a list_t pointer to points the head
  *@str: Pointer to string
  *Return: Pointer to new head
@@ -16,10 +16,21 @@ list_t *add_node(list_t **head, const char *str)
 
 	while (str[length])
 		length++;
+/*
+ * To obtain the str length and store in "length"
+ */
 
 	tmp->len = length;
 	tmp->str = strdup(str);
 	tmp->next = *head;
 	*head = tmp;
+/*
+ * Assign the length of the string to the 'len' field of the node.
+ * Assign a copy of the string 'str' to the 'str' field of the node using the
+ * strdup function of lib <string.h>
+ * Set the 'next' pointer of the new node to point to the current first node.
+ * he pointer to the first node (head) is updated to point to the newly
+ * added node.
+ */
 	return (tmp);
 }
