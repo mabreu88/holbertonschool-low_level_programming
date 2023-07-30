0d48a35fb77411436df8e894944a311f184ccdac
mabreu88 <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@github.com>
2023-07-30 15:11:43 -0700

    1. 1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -5,17 +5,19 @@
  */
 void print_binary(unsigned long int n)
 {
-	signed long int max = sizeof(n) * 8 - 1;
-	unsigned int x = 0;
+	signed long int size = sizeof(n) * 8 - 1;
+	int flag = 0;
+
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	while (max >= 0)
+
+	while (size >= 0)
 	{
-	x = ((n >> max) & 1) ? 1 : (x ? _putchar('0') : 0);
-		_putchar(((n >> max) & 1) ? '1' : 0);
-		max--;
+	flag = ((n >> size) & 1) ? 1 : (flag ? _putchar('0') : 0);
+		_putchar(((n >> size) & 1) ? '1' : 0);
+		size--;
 	}
 }
