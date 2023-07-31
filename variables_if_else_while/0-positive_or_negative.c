fc8c4e4c658388a8430be0f9734e3bbd1772f67a
mabreu88 <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@github.com>
2023-07-31 10:33:49 -0700

    Add comments

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,31 @@
 int main(void)
 {
 	int n;
-
+/*
+ * Initialize the random number generator with a seed based on the current time
+ */
 	srand(time(0));
+/*
+ * Generate a random number in the range of -RAND_MAX/2 to RAND_MAX/2
+ */
 	n = rand() - RAND_MAX / 2;
+/*
+ * Check if the number is positive
+ */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
+/*
+ * Check if the number is zero
+ */
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
+/*
+ * If the number is not positive or zero, it must be negative
+ */
 	else
 	{
 		printf("%d is negative\n", n);
