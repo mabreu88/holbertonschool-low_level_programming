3bd279f7f521457837b616897475c97ceba77af5
mabreu88 <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@github.com>
2023-07-31 10:22:01 -0700

    Add comments

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,6 +6,14 @@
  */
 int main(void)
 {
+/*
+ * The sizeof function return the size on bytes of diferent types of variables
+ * char 8 bits, unsigned char 8 bits, signed char 8 bits, int 16 bits, unsigned
+ * int 16 bit, signed int 16 bit, short int 8 bits, unsigned short int 8 bits,
+ * signed short int 8 bits,  long int 32 bit, signed long int 32 bits,
+ * unsigned long int 32 bits, float 32 bits, double 64 bits,
+ * long double 128 bits.
+ */
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
