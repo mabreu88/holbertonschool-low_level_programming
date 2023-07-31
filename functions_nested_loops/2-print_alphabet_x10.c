dcad88fc7fb830f8f7db71ac842a38456f0f9aab
mabreu88 <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@github.com>
2023-07-31 12:44:50 -0700

    Add comments

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,11 @@ void print_alphabet_x10(void)
 {
 	char x;
 	int count;
-
+/*
+ * Generate two diferents for structures. The first is used as a counter,
+ * each time it is incremented, the second for is executed which prints
+ * the entire alphabet ending with a line break.
+ */
 	for (count = 0; count <= 9; count++)
 	{
 
