f6a8235cf84f2fd7390f554483f60ec9eff26ff7
mabreu88 <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@github.com>
2023-07-31 11:49:57 -0700

    Add comments

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,7 +8,10 @@ int main(void)
 {
 	char low;
 	int x;
-
+/*
+ * Generate two diferents for structures, one for print all base 10 numbers
+ * 0 to 9, and other to print letter form 'a' to 'f'.
+ */
 	for (x = '0'; x <= '9'; x++)
 		putchar(x);
 	for (low = 97; low <= 102; low++)
