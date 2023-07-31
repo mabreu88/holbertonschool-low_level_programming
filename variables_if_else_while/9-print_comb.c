62a442dfa63eb96cc105081d4150a5696412410e
mabreu88 <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@github.com>
2023-07-31 11:54:21 -0700

    Add comments

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,10 +7,15 @@
 int main(void)
 {
 	int x;
-
+/*
+ * Use for structure to print al base 10 digits form 0 to 9.
+ */
 	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
+/*
+ * Each time, if the number is different to 9 print ',' an ' '
+ */
 		if (x != '9')
 		{
 			putchar(',');
