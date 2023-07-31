4b82ee07ecb4594f59d04e2a9ee64cc5a1d33e6d
mabreu88 <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@github.com>
2023-07-31 12:04:17 -0700

    Add comments

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,7 +6,10 @@ void print_alphabet(void)
 {
 
 	char x;
-
+/*
+ * With a for structure increase 'x' each time, to print alphabet, use 
+ * the _putchar function to print a single chatacter.
+ */
 	for (x = 'a'; x <= 'z'; x++)
 		_putchar(x);
 	_putchar('\n');
