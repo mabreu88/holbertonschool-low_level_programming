faf310a772336513ebf25cf90cbc555334b1c392
mabreu88 <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@github.com>
2023-07-30 19:26:08 -0700

    fix long error

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,9 +9,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int max = 0x01;
 
-	max = ~(max << index);
-	if (max == 0x00)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	*n &= max;
+
+	max <<= index;
+	*n &= ~max;
 	return (1);
 }
