5fad6303bdfa4fc96811e29c75daf7ce7bc1fce2
mabreu88 <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@github.com>
2023-07-31 10:12:04 -0700

    Add comments

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -6,6 +6,10 @@
  */
 int main(void)
 {
+/*
+ * Use printf to print the line, printf is a function included in the stdio.h
+ * lib.
+ */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
